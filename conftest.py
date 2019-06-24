f6d3aa3eed79c820ea92da6dd911fb5b90019a15
Nikkey78 <nick-bazh@yandex.ru>
2019-06-24 15:23:26 +0300

    stepik_3_6_9

--- conftest.py
+++ conftest.py
@@ -1,11 +1,15 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope="function")
 def browser():
+    options = Options()
+    user_langs = ''
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_langs})
     print("\nstart browser for test..")
-    browser = webdriver.Chrome()
+    browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
